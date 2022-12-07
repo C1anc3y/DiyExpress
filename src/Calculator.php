2fad3008773075e2f6e29c7d9d42186729dc82d4
ClanceyHuang <ClanceyHuang@outlook.com>
2022-12-07 22:58:56 +0800

    feat: 计算结果可以不是数值，即原来验证的算术表达式，现在突破到逻辑表达式

--- src/Calculator.php
+++ src/Calculator.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @File    :   Calculator.php
  * @Author  :   ClanceyHuang
@@ -11,6 +12,7 @@
 
 
 namespace DiyExpress;
+
 use DiyExpress\AST\NodeType;
 use DiyExpress\Formula\SimpleFormula;
 use DiyExpress\Parser\SimpleParser;
@@ -18,7 +20,7 @@ use DiyExpress\VM\SimpleVM;
 
 class Calculator
 {
-    CONST MAX_LEVEL = 1000; # 最深层级，执行到该层级还未计算出结果，直接 return 0
+    const MAX_LEVEL = 1000; # 最深层级，执行到该层级还未计算出结果，直接 return 0
 
     /**
      * 验证表达式
@@ -26,51 +28,52 @@ class Calculator
      * @return array|false
      * @throws \Exception
      */
-    public function verifyFormFormula($script_list){
+    public function verifyFormFormula($script_list)
+    {
         if (!$script_list) {
             return false;
         }
         $result = [];
         foreach ($script_list as $index => &$script) { # 任意一条出错，直接返回错误提示
-            $script = str_replace('\'',"'", $script);
-            $script = str_replace('\"','"',$script);
+            $script = str_replace('\'', "'", $script);
+            $script = str_replace('\"', '"', $script);
             # 必须要进入循环重新实例化，否则原有的数据会对后面的公式对象造成干扰。
             $parser = new SimpleParser();
             $vm = new SimpleVM();
             $nodeList = null;
             $nodeList = $parser->parse($script);
             $res_field = $nodeList['field'];
-            if ($res_field){
+            if ($res_field) {
                 $script_key = array_shift($res_field);
                 $result[$script_key] = [
-                    'formula'=>$script,
-                    'child'=>$nodeList['field']
+                    'formula' => $script,
+                    'child' => $nodeList['field']
                 ];
             }
             # 验证用的随机给变量赋值
             $check_data_list = [];
-            foreach ($nodeList['data'] as $node_v){
-                if (in_array($node_v->structText,$res_field)){
-                    if (!isset($check_data_list[$node_v->structText])){
-                        $rand = rand(100,999); # 构造随机验证数据
+            foreach ($nodeList['data'] as $node_v) {
+                if (in_array($node_v->structText, $res_field)) {
+                    if (!isset($check_data_list[$node_v->structText])) {
+                        $rand = rand(100, 999); # 构造随机验证数据
                         $check_data_list[$node_v->structText] = $rand;
                         $node_v->structText = $rand;
-                    }else{
+                    } else {
                         $node_v->structText = $check_data_list[$node_v->structText];
                     }
                 }
             }
             unset($res_field);
             $vmRes = $vm->scanNodeList($nodeList['data']);
-            if ($vmRes['error_msg']){
+            if ($vmRes['error_msg']) {
                 $result =  false;
             }
             $treeList = $vmRes['nodeList'];
             $treeList = $this->dealWithAdapt($treeList);
-            if (!$treeList){
-                $result=false;
+            if (!$treeList) {
+                $result = false;
             }
-            if (!$result){
+            if (!$result) {
                 break;
             }
             unset($script);
@@ -85,9 +88,9 @@ class Calculator
      * @param string|int $rule_result_filed_id 表达式的结果字段id或者标识
      * @return mixed
      */
-    public function executeFormFormula($token, $data,$rule_result_field_id)
+    public function executeFormFormula($token, $data, $rule_result_field_id)
     {
-        
+
         $result = 0;
         if (!$token || !$data || !$rule_result_field_id) {
             return $result;
@@ -98,24 +101,22 @@ class Calculator
         $res_str = $token;
 
         # 先把结果标记处理掉
-        $res_str = str_replace('{{'.$rule_result_field_id.'}}','结果',$res_str);
+        $res_str = str_replace('{{' . $rule_result_field_id . '}}', '结果', $res_str);
         // breakpoint log，输出替换结果标识后的表达式
-        var_dump($res_str);
-        foreach ($data as $key=> $val){
-            $res_str = str_replace('{{'.$key.'}}',$val?:0,$res_str);
+        foreach ($data as $key => $val) {
+            $res_str = str_replace('{{' . $key . '}}', $val ?: 0, $res_str);
         }
-        // breakpoint log , 输出替换变量后的表达式
-        var_dump($res_str);
-        
         $nodeList = $parser->parse($res_str);
         $vmRes = $vm->scanNodeList($nodeList['data']);
-        if ($vmRes['error_msg']){
+        if ($vmRes['error_msg']) {
             return $result;
         }
 
         $treeList = $vmRes['nodeList'];
         $treeList = $this->dealWithAdapt($treeList);
-        if (is_numeric($treeList)){
+        if (is_numeric($treeList)) {
+            $result = $treeList;
+        } elseif (!is_null($treeList)) {
             $result = $treeList;
         }
         return $result;
@@ -126,18 +127,21 @@ class Calculator
      * @param $nodeList
      * @return int|string
      */
-    public function dealWithAdapt(&$nodeList){
+    public function dealWithAdapt(&$nodeList)
+    {
         $max_level = self::MAX_LEVEL;
-        for ($i=0;$i<=$max_level;$i++){
-            if (is_object($nodeList) || (is_array($nodeList) && count($nodeList)>1)){
+        for ($i = 0; $i <= $max_level; $i++) {
+            if (is_object($nodeList) || (is_array($nodeList) && count($nodeList) > 1)) {
                 $nodeList = $this->dealWithOpera($nodeList);
-            }else{
+            } else {
                 break;
             }
         }
-        if (is_numeric($nodeList)){
-            return $nodeList?:0;
-        }else{
+        if (is_numeric($nodeList)) {
+            return $nodeList ?: 0;
+        } elseif (is_string($nodeList)) {
+            return $nodeList ?: "";
+        } else {
             return 0;
         }
     }
@@ -149,60 +153,59 @@ class Calculator
      * @param int $level
      * @return array|int
      */
-    public function dealWithOpera(&$nodeList,$level=20){
+    public function dealWithOpera(&$nodeList, $level = 20)
+    {
         $astNodeType = new NodeType();
         $simpleScript = new SimpleFormula();
         $first_cal_tag = 1;
         $first_op_list  = [];
 
-        if(is_array($nodeList)){
-            foreach($nodeList as $first_k => &$first_v){
-                if (is_numeric($first_k)){
-                    if (is_array($first_v)){
+        if (is_array($nodeList)) {
+            foreach ($nodeList as $first_k => &$first_v) {
+                if (is_numeric($first_k)) {
+                    if (is_array($first_v)) {
                         $first_cal_tag = 0;
-                        if (count($first_v)==1){
+                        if (count($first_v) == 1) {
                             $first_v = array_pop($first_v);
-                        }else{
+                        } else {
                             $first_v = $this->dealWithOpera($first_v);
                         }
-
-                    }
-                    elseif(is_object($first_v)){
+                    } elseif (is_object($first_v)) {
                         $first_v_text = $first_v->structText;
-                        if (is_array($first_v_text)){
+                        if (is_array($first_v_text)) {
                             $first_cal_tag = 0;
-                            if (count($first_v_text)==1){
+                            if (count($first_v_text) == 1) {
                                 $first_v->structText = array_pop($first_v_text);
                             }
                             $first_v->structText = $this->dealWithOpera($first_v->structText);
-                        }else{
-                            if ($first_cal_tag){
+                        } else {
+                            if ($first_cal_tag) {
                                 $first_op_list[] = $first_v;
-                            }else{
+                            } else {
                                 $first_op_list = []; # 清空
                             }
                         }
-                    }elseif(is_numeric($first_v)){
-                        $first_op_list[] = (object)['structType'=>'RESULT','structText'=>$first_v];
-                    }else{
+                    } elseif (is_numeric($first_v)) {
+                        $first_op_list[] = (object)['structType' => 'RESULT', 'structText' => $first_v];
+                    } else {
                         continue;
                     }
-                }else{
+                } else {
                     continue;
                 }
             }
-            if ($first_cal_tag){
+            if ($first_cal_tag) {
                 # 没有下级，直接计算当前的字面量
                 $first_op_tag_data = $first_op_list[1];
                 $first_op_left_data = $first_op_list[0];
                 $first_op_right_data = $first_op_list[2];
                 $first_op_tag_type = $first_op_tag_data->structType;
-                if (in_array($first_op_tag_type,array_merge($astNodeType->high_tag,$astNodeType->low_tag,$astNodeType->compare_tag,$astNodeType->logic_tag))){
+                if (in_array($first_op_tag_type, array_merge($astNodeType->high_tag, $astNodeType->low_tag, $astNodeType->compare_tag, $astNodeType->logic_tag))) {
                     $first_op_left_text = $first_op_left_data->structText;
                     $first_op_right_text = $first_op_right_data->structText;
                     $first_res = $simpleScript->calculator(
                         $first_op_tag_type,
-                        ['left'=>$first_op_left_text,'right'=>$first_op_right_text]
+                        ['left' => $first_op_left_text, 'right' => $first_op_right_text]
                     );
                     $nodeList = $first_res;
                 }
@@ -211,65 +214,76 @@ class Calculator
             // # 循环处理if的结果
             $if_cal_tag = 1;
             $if_op_list  = [];
-            foreach($nodeList as $if_k => &$if_v){
-                if (in_array($if_k,['condition','then','else'])){
-                    if (is_array($if_v)){
+            foreach ($nodeList as $if_k => &$if_v) {
+                if (in_array($if_k, ['condition', 'then', 'else'])) {
+                    if (is_array($if_v)) {
                         $if_cal_tag = 0;
-                        if (count($if_v)==1){
+                        if (count($if_v) == 1) {
                             $if_v = array_pop($if_v);
                         }
                         $if_v = $this->dealWithOpera($if_v);
-                    }
-                    elseif(is_object($if_v)){
+                    } elseif (is_object($if_v)) {
                         $if_v_text = $if_v->structText;
-                        if (is_array($if_v_text)){
+                        if (is_array($if_v_text)) {
                             $if_cal_tag = 0;
                             $if_v->structText = $this->dealWithOpera($if_v->structText);
-                        }else{
-                            if (!$if_cal_tag){
+                        } else {
+                            if (!$if_cal_tag) {
                                 $if_op_list = [];
-                            }
-                            else{
+                            } else {
                                 $if_op_list[$if_k] = $if_v;
                             }
                         }
-                    }elseif(is_numeric($if_v)){
-                        $if_op_list[$if_k] = (object)['structType'=>'RESULT','structText'=>$if_v];
-                    }else{
+                    } elseif (is_numeric($if_v)) {
+                        $if_op_list[$if_k] = (object)['structType' => 'RESULT', 'structText' => $if_v];
+                    } else {
                         continue;
                     }
                 }
             }
-            if ($if_cal_tag){
+
+            if (
+                is_array($nodeList)
+                && count($nodeList) == 3
+                && isset($nodeList['condition'])
+                && !isset($nodeList['condition'][0])
+                && isset($nodeList['then'])
+                && isset($nodeList['else'])
+            ) {
+                if ($nodeList['condition']) {
+                    $nodeList = $nodeList['then'];
+                } else {
+                    $nodeList = $nodeList['else'];
+                }
+            }
+            if ($if_cal_tag) {
                 # 没有下级，直接计算当前的字面量
-                $if_condition = $if_op_list['condition']?:null;
-                $if_then = $if_op_list['then']?:null;
-                $if_else = $if_op_list['else']?:null;
-                if (!is_null($if_condition) && !is_null($if_then) && !is_null($if_else)){
+                $if_condition = $if_op_list['condition'] ?: null;
+                $if_then = $if_op_list['then'] ?: null;
+                $if_else = $if_op_list['else'] ?: null;
+                if (!is_null($if_condition) && !is_null($if_then) && !is_null($if_else)) {
                     $condition_text = $if_condition->structText;
                     $then_text = $if_then->structText;
                     $else_text = $if_else->structText;
-                    $if_res = $simpleScript->calculator($astNodeType::IF_STMT,[
-                        'condition'=>$condition_text,
-                        'then'=>$then_text,
-                        'else'=>$else_text,
+                    $if_res = $simpleScript->calculator($astNodeType::IF_STMT, [
+                        'condition' => $condition_text,
+                        'then' => $then_text,
+                        'else' => $else_text,
                     ]);
                     $nodeList = $if_res;
                 }
             }
-        }elseif(is_object($nodeList)){
+        } elseif (is_object($nodeList)) {
             $first_v_text = $nodeList->structText;
-            if (is_array($first_v_text)){
-                if (count($first_v_text)==1){
+            if (is_array($first_v_text)) {
+                if (count($first_v_text) == 1) {
                     $nodeList = array_pop($first_v_text);
                 }
                 $nodeList = $this->dealWithOpera($first_v_text);
-            }else{
+            } else {
                 $nodeList = $nodeList->structText;
             }
         }
-
         return $nodeList;
     }
-
 }
--- tests/test.php
+++ tests/test.php
@@ -11,13 +11,15 @@
 error_reporting(E_ERROR);
 require __DIR__ . '/../vendor/autoload.php';
 
-// demo express.
-$script_list = [
-    'y=if((x+30)*0.5>100,x+60,x-90)+50',
-];
+// // demo express.
+// $script_list = [
+//     'y=if((x+30)*0.5>100,x+60,x-90)+50',
+//     "z=if(((x+30)*0.5)>100,'真值','假值')"
+// ];
 $caculatorCls = new DiyExpress\Calculator();
-$result = $caculatorCls->verifyFormFormula($script_list);
-var_dump($result);
+// $result = $caculatorCls->verifyFormFormula($script_list);
+// var_dump($result);
+// die;
 // array(1) {
 //     ["y"]=>
 //     array(2) {
@@ -40,10 +42,20 @@ var_dump($result);
 // ];
 
 $data = [
-    'x'=>30
+    'x'=>171
 ];
-$dirty_express = 'y=if((x+30)*0.5>100,x+60,x-90)+50'; // 原本的自定义表达式，在使用计算的时候，需要特殊处理一下，将变量用双花括号包起来，如下：
-$diy_express = '{{y}}=if(({{x}}+30)*0.5>100,{{x}}+60,{{x}}-90)+50';
-$res = $caculatorCls->executeFormFormula($diy_express,$data,'y');
-var_dump($res);
+var_dump($data);
+
+// $dirty_express1 =  "y=if((x+30)*0.5>100,x+60,x-90)+50";
+// $diy_express1 = "{{y}}=if((({{x}}+30)*0.5)>100,{{x}}+60,{{x}}-90)+50";
+// var_dump($diy_express1);
+// $res = $caculatorCls->executeFormFormula($diy_express1,$data,'y');
+// var_dump(['y'=>$res]);
+
+$dirty_express = "z=if(((x+30)*0.5)>100,'真值','假值')"; // 原本的自定义表达式，在使用计算的时候，需要特殊处理一下，将变量用双花括号包起来，如下：
+$diy_express = "{{z}}=if((({{x}}+30)*0.5)>100,'真值','假值')";
+var_dump($diy_express);
+
+$res = $caculatorCls->executeFormFormula($diy_express,$data,'z');
+var_dump(['z'=>$res]);
 die;
